12bd3871faa739611013f7a52e39d99515cd80ae
danzel-py <danzelrtm@gmail.com>
2020-10-22 17:45:33 +0700

    Static Box #1
    Added a Static Box (unfinished)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <QuoteBox id="quote-box" />
       </header>
     </div>
   );
 }
+
+
+class QuoteBox extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: null
+    }
+  }
+
+  render(){
+    return(
+      <div id="boxbuatquoteauth">
+        <QuotesAndAuthor />
+        <Navbwh />
+      </div>
+    )
+  }
+}
+
+class QuotesAndAuthor extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quotesnya: '"Ini quotesnya"',
+      author: '-Ini yg ngomong'
+    }
+  }
+  
+  render(){
+    return(
+      <div>
+        <div id="quote-text">
+          {this.state.quotesnya}
+        </div>
+        <div id="author">
+          {this.state.author}
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+const Navbwh = () => {
+  return(
+    <div id='navbwh'>
+        
+    </div>
+  )
+  
+}
+
+const getRandomNumber = (n) => {
+  return Math.floor(Math.random()*n)
+}
+
+const quotesList = [
+  {
+    text: 'Queen Elsa, make a prince. A fancy one.',
+    author: 'Anna'
+  }
+]
 
 export default App;
