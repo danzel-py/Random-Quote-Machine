c0cd85bbe278dfd517228b43e6e7469f382f4eb7
danzel-py <danzelrtm@gmail.com>
2020-10-23 22:09:49 +0700

    Point of Realization
    At this point i realize i didn't plan my react component carefully, therefore my components are messy af. I'll rage quit and startover with careful planning. :(

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
 function App() {
@@ -13,59 +12,73 @@ function App() {
 }
 
 
-class QuoteBox extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      quote: null
-    }
-  }
-
-  render(){
+const QuoteBox = () => {
     return(
       <div id="boxbuatquoteauth">
-        <QuotesAndAuthor />
+        <QuotesAndAuthor quotesnya='' author='' />
         <Navbwh />
       </div>
-    )
-  }
+    )  
 }
 
-class QuotesAndAuthor extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      quotesnya: '"Ini quotesnya"',
-      author: '-Ini yg ngomong'
-    }
-  }
-  
-  render(){
+const QuotesAndAuthor = (props) =>{
     return(
       <div>
         <div id="quote-text">
-          {this.state.quotesnya}
+          {props.quotesnya}
         </div>
         <div id="author">
-          {this.state.author}
+          {props.author}
         </div>
         
       </div>
     )
   }
-}
 
-const Navbwh = () => {
-  return(
+function Navbwh() {
+  return (
     <div id='navbwh'>
-        
+      <div>
+        <a href="https:\\www.twitter.com" id="tweet-quote">logo tweet</a>
+      </div>
+      <div>
+        <a href="https:\\www.fb.com" id="fb-quote">logo fb</a>
+      </div>
+      <NewQuote />
     </div>
-  )
-  
+  );
+
 }
 
-const getRandomNumber = (n) => {
-  return Math.floor(Math.random()*n)
+class NewQuote extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quotes: '',
+      auth: ''
+    }
+    this.generateRandomQuote = this.generateRandomQuote.bind(this)
+  }
+
+  generateRandomQuote(){
+    let x = getRandomNumber(1)
+    this.setState({quotes: quotesList[x].text, auth: quotesList[x].author})
+  }
+  
+
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.generateRandomQuote}>New Quote</button>
+        <p>{this.state.quotes}-{this.state.auth}</p>
+      </div>
+    )
+  }
+}
+
+const getRandomNumber = (numbers) => {
+  return Math.floor(Math.random()*numbers)
 }
 
 const quotesList = [
