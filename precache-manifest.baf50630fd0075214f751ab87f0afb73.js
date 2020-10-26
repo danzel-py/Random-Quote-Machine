157fe22d9b89e3c8d50bc95c36bf5cbb2340eaf3
danzel-py <danzelrtm@gmail.com>
2020-10-26 11:58:46 +0700

    Updates

---
+++ precache-manifest.baf50630fd0075214f751ab87f0afb73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6444dcc53e7d1f80299ddf9d322b110",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "280b277cbbb00b2c859c",
+    "url": "/Random-Quote-Machine/static/css/main.3af652e5.chunk.css"
+  },
+  {
+    "revision": "5bfe2c85fc7fd246db8f",
+    "url": "/Random-Quote-Machine/static/js/2.7b0982b7.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Random-Quote-Machine/static/js/2.7b0982b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280b277cbbb00b2c859c",
+    "url": "/Random-Quote-Machine/static/js/main.8eaadd33.chunk.js"
+  },
+  {
+    "revision": "857d28a68373431d086b",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.ae688bef.js"
+  }
+]);
